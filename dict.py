30dc92d1b0bf29859814c42a1aae392bf1a387f5
LI474525323 <55075770+LI474525323@users.noreply.github.com>
2021-09-07 16:00:18 +0800

    向空字典增加数值

--- dict.py
+++ dict.py
@@ -1,3 +1,4 @@
+        hashTable = {}
         for a in nums1:
             for b in nums2:
                 if a+b not in HashTable:  # 也可以if HashTable.get(a+b) == None:  HashTable(a+b)=1 #也可一if a+b not in HashTable.keys():
